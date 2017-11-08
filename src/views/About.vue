f9f7bb61a25a13c25b0453e02b42a7df73ea33db
guitong <guitong@gmail.com>
2017-11-08 11:06:38 +0800

    add Vuex

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="about">
+    <h1>About Me</h1>
+    <h2>Test</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  created () {
+    console.log('created')
+  },
+  mounted () {
+    console.log('mounted')
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,15 +1,46 @@
 <template>
   <div id="view-movie">
     <h1>Status</h1>
+    <ul>
+      <li>{{ count }}</li>
+    </ul>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+    <p>
+      <button>Square</button>
+    </p>
   </div>
 </template>
 
 <script>
 // import ajax from '@/util/ajax'
+import { mapState } from 'Vuex'
 export default {
   name: 'movie',
+  data () {
+    return {
+      localCount: 0
+    }
+  },
   mounted () {
     console.log('status mounted')
+  },
+  computed: mapState({
+    count: state => state.count,
+    countAlias: 'count',
+    countPlusLocalState (state) {
+      return state.count + this.localCount
+    }
+  }),
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    },
+    decrement () {
+      this.$store.commit('decrement')
+    }
   }
 }
 </script>
